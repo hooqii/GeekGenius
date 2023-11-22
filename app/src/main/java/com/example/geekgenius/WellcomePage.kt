34b4c57dab1cb787a5140107d5514814da3b242d
hooqii <hooqii.me@gmail.com>
2023-11-22 23:58:53 +0700

    Added - Logo App - Splash Screen - Front End Wellcome-Page - Front End Login-Page - Front End Register-Page

--- app/src/main/java/com/example/geekgenius/WellcomePage.kt
+++ app/src/main/java/com/example/geekgenius/WellcomePage.kt
@@ -2,10 +2,23 @@ package com.example.geekgenius
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class WellcomePage : AppCompatActivity() {
+
+    //Binding
+//    private lateinit var binding :
+
+    private lateinit var btnLoginGoogle : Button
+    private lateinit var btnLoginEmail : Button
+    private lateinit var textLinkRegister : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wellcome_page)
+
+        btnLoginGoogle = findViewById(R.id.btnLoginGoogle)
+        btnLoginEmail = findViewById(R.id.btnLoginEmail)
+
     }
 }
