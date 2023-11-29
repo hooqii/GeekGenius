c2e6dd684d2b0ab24adc1c45692e7f0cc6269908
hooqii <hooqii.me@gmail.com>
2023-11-30 03:40:11 +0700

    Added Info & Detail Kampus Pages

---
+++ app/src/main/java/com/example/geekgenius/DetailKampusPage.kt
@@ -0,0 +1,11 @@
+package com.example.geekgenius
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailKampusPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_kampus_page)
+    }
+}
---
+++ app/src/main/java/com/example/geekgenius/InfoKampusPage.kt
@@ -0,0 +1,11 @@
+package com.example.geekgenius
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoKampusPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_kampus_page)
+    }
+}
